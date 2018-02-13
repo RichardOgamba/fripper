c855c60c821a9cba43a7f81c6de332071d48b892
Richard Ogamba <ogambarichard20@gmail.com>
2018-02-13 16:44:53 +0300

    initialize the app

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_socket import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+from app import views
