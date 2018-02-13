fe820d74a5ffc376fe9171597af65c573924a78d
Richard Ogamba <ogambarichard20@gmail.com>
2018-02-13 17:22:57 +0300

    move run function to run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from flask_socket import SocketIO
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
 from app import views
